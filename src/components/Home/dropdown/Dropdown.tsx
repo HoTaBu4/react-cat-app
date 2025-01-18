c6394dc2866a0073c0fb8124a58c4fa2ece9cd4e
vova <hohiop09@gmail.com>
2025-01-18 15:32:28 +0200

    correct the dropdown

--- src/components/Home/dropdown/Dropdown.tsx
+++ src/components/Home/dropdown/Dropdown.tsx
@@ -35,7 +35,7 @@ const DropDown: React.FC<DropDownButtonProps> = ({
         </button>
 
         {isOpen && (
-          <div className="absolute h-80 left-0 z-10 mt-2 w-56 origin-top-right rounded-md bg-white shadow-lg ring-1 ring-black/5 focus:outline-none">
+          <div className="absolute h-80 left-0 z-10 bg-white mt-2 w-56 origin-top-right rounded-md shadow-lg ring-1 ring-black/5 focus:outline-none">
             <div className="py-1 h-80 overflow-y-auto">
               {list.map((item) => (
                 <label
